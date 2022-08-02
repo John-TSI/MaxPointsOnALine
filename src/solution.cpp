55bbfd86f9d38c7c388862f15c2ca32fb448c917
John Roughley <jroughley@softwareinstitute.com>
2022-08-02 13:55:44 +0100

    removed unnecessary if condition, added comments

--- src/solution.cpp
+++ src/solution.cpp
@@ -7,33 +7,36 @@
 
 int Solution::maxPoints(std::vector<std::vector<int>>& points)
 {
-    std::map<std::pair<float,float>, int> lineMap{{}};
-    std::vector<std::tuple<float,float,int>> tupleVec;
-    float grad, icpt;
+    float grad, icpt;  // gradient and y-intercept of line connnecting two points
+    std::map<std::pair<float,float>, int> lineMap{{}};  // maps pairs of line params (grad, icpt) to an occurrence counter
+    std::vector<std::tuple<float,float,int>> tupleVec;  // to avoid overcounting, maintain a vector of tuples of grad, icpt, and a y-value
 
     for(int i{0}; i<points.size()-1; ++i)
     {
-        for(int j{i+1}; j<points.size(); ++j)
+        for(int j{i+1}; j<points.size(); ++j)  // consider a line connecting every pair of points in turn
         {
             if(points[j][0] == points[i][0]){ grad = INT_MAX; icpt = INT_MIN; }
             else
             { 
-                grad = static_cast<float>(points[j][1] - points[i][1]) / (points[j][0] - points[i][0]);
-                icpt = points[i][1] - grad*points[i][0]; 
+                grad = static_cast<float>(points[j][1] - points[i][1]) / (points[j][0] - points[i][0]);  // grad = Δy / Δx
+                icpt = points[i][1] - grad*points[i][0];  // icpt = y - (grad * x) for either of the two points
             }
 
             auto lineParams = std::make_pair(grad, icpt);
             auto tupleParams = std::make_tuple(grad, icpt, points[i][1]);
-            if( !lineMap.contains(lineParams) || std::find(tupleVec.begin(), tupleVec.end(), tupleParams) == tupleVec.end() )
+            // insert lineParams and increment its count only if the tuple containing the y1-value used to extract the params does not exist
+            if( std::find(tupleVec.begin(), tupleVec.end(), tupleParams) == tupleVec.end() )
             { 
-                lineMap[lineParams] += 1; tupleVec.push_back(tupleParams);
+                lineMap[lineParams] += 1; tupleVec.push_back(tupleParams);  // insert tuple into tupleVec to prevent overcounting
             }
         }
     }
 
     auto maxCountIt = std::max_element
     (
+        // define a custom exeecution policy using a lambda to define how comparisons using < operator should be carried out
         lineMap.begin(), lineMap.end(), [](const auto& p1, const auto& p2){ return p1.second < p2.second; }
     );
+    // lineMap has actually counted number of non-overlapping 'sub-lines' on each line, equal to one less than number of distinct points on the line 
     return maxCountIt->second + 1;
 }
