91b2d8cf9cff5bbe17b2097da967c3806db6c2c9
John Roughley <jroughley@softwareinstitute.com>
2022-08-02 12:22:37 +0100

    completed leetcode exercise 149

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,8 @@
 
 int main()
 {
-	std::vector<std::vector<int>> points{{1,1},{3,2},{5,3},{4,1},{2,3},{1,4}};
+	//std::vector<std::vector<int>> points{{1,1},{2,2},{3,3}};  // 3
+	std::vector<std::vector<int>> points{{1,1},{3,2},{5,3},{4,1},{2,3},{1,4}};  // 4
 	Solution sol;
 	std::cout << sol.maxPoints(points);
 	return 0;
--- src/solution.cpp
+++ src/solution.cpp
@@ -1,28 +1,39 @@
 #include"../inc/solution.hpp"
+#include<map>
+#include<tuple>
 #include<climits>
+#include<algorithm>
 
 
 int Solution::maxPoints(std::vector<std::vector<int>>& points)
 {
-    std::vector<std::pair<float,float>> params;  // will hold gradient and y-intercept for lines connecting each pair of points
+    std::map<std::pair<float,float>, int> lineMap{{}};
+    std::vector<std::tuple<float,float,int>> tupleVec;
+    float grad, icpt;
+
     for(int i{0}; i<points.size()-1; ++i)
     {
         for(int j{i+1}; j<points.size(); ++j)
         {
-            float grad = (points[j][0] == points[i][0])
-                        ? INT_MAX
-                        : (float) (points[j][1] - points[i][1]) / (points[j][0] - points[i][0]);
-            float icpt = (points[j][0] == points[i][0])
-                        ? INT_MIN
-                        : (float) points[i][1] - grad*points[i][0];
-            params.push_back(std::make_pair(grad,icpt));
+            if(points[j][0] == points[i][0]){ grad = INT_MAX; icpt = INT_MIN; }
+            else
+            { 
+                grad = static_cast<float>(points[j][1] - points[i][1]) / (points[j][0] - points[i][0]);
+                icpt = points[i][1] - grad*points[i][0]; 
+            }
+
+            auto lineParams = std::make_pair(grad, icpt);
+            auto tupleParams = std::make_tuple(grad, icpt, points[i][1]);
+            if( !lineMap.contains(lineParams) || std::find(tupleVec.begin(), tupleVec.end(), tupleParams) == tupleVec.end() )
+            { 
+                lineMap[lineParams] += 1; tupleVec.push_back(tupleParams);
+            }
         }
     }
-    int maxCount{0};
-    for(auto vals : params)
-    {
-        int thisCount = std::count(params.begin(), params.end(), vals); 
-        if(thisCount > maxCount){ maxCount = thisCount; }
-    }
-    return maxCount;
+
+    auto maxCountIt = std::max_element
+    (
+        lineMap.begin(), lineMap.end(), [](const auto& p1, const auto& p2){ return p1.second < p2.second; }
+    );
+    return maxCountIt->second + 1;
 }
